d3719f35175b36517b821c908bc3b00321e03cb2
Atsushi Yamagishi <haru.su.jp@gmail.com>
2014-05-25 11:17:47 +0900

    removed ticks, added some comments. This has to become more flexible

--- envelope_theorem_ver0.1.py
+++ envelope_theorem_ver0.1.py
@@ -7,7 +7,7 @@ from __future__ import division
 def f(x):
 	return x**2  #sample
 
-def slope(f,a):
+def slope(f,a): #approximating the slope of a tangent line
 	return (f(a+0.0001)-f(a))/0.0001
 	
 def subplots():
@@ -22,17 +22,20 @@ def subplots():
     
     return fig, ax
 
+
 fig, ax = subplots()  # Call the local version, not plt.subplots()
+ax.tick_params(which="both",bottom="off",top="off",left="off",right="off",labelbottom="off",labeltop="off",labelleft="off",labelright="off")
+
 x = np.linspace(-5, 5, 200)
 y = f(x)
 ax.plot(x, y, 'b-', linewidth=2)
 
 t=[]
 for i in range(-10,11):
-	t.append(0.5*i)
+	t.append(0.5*i) #You wanna change the numbers in this "for loop" when you modify this program.
 	
 for i in t:
 	tan=x*slope(f,i)+f(i)-slope(f,i)*i
-	ax.plot(x,tan,"b-",linewidth=1)
+	ax.plot(x,tan,"b-",linewidth=1) #drawing tangent lines
 	
 plt.show()
